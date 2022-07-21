71db17f80e709cb4efb505be740f573539c32938
Paul Jung <pbnjcub@gmail.com>
2022-07-21 14:48:49 -0400

    commit tree 8ea2448ba2973f5a35adc826d3314885fea6e146

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// }
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+// }
+
+//   fetch('http:/localhost:3000/dogs', configurationObject)
+//   .then(function (response) {
+//     return response.json()
+//   })
+//   .then(function (object) {
+//     console.log(object)
+//   })
+//   .catch(function (error) {
+//     alert("Bad things! Ragnarok!")
+//     console.log(error.message)
+//   })
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email:`${email}`,
+        })
+    })
+        .then(response => response.json())
+        .then(user => {
+            let body = document.querySelector('body')
+            body.innerHTML =`
+                <p>${user.id}</p>
+                `
+        })
+        .catch(error => {
+            let body = document.querySelector('body')
+            body.innerHTML =`
+                <p>${error.message}</p>
+                `
+        })
+   
+}
